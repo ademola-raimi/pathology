d57ca6b75a8ac9b566892e01e3601d3b40616f4c
Ademola <ademola.raimi@andela.com>
2017-05-13 17:24:44 +0100

    incorrect display fix in landing page

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -8,27 +8,29 @@
                     <div class="container">
                         <div class="row">
                             <div class="intro-text">
-                                <h3>Find a medical expert to run medical test for you - One click away</h3>
-                                <p>Have you recently been tested? <br> Click Patient Log in to view your report</p>
                                 @if (! Auth::check())
-                                <a href="{{ route('getSignup') }}" class="page-scroll btn btn-primary mobile-btn-hov" style="background-color: #3B5998; width: 20%; border: none;">
-                                    <i class="fa fa-user-plus"></i> Staff Sign Up
-                                </a>
+                                    <h3>Find a medical expert to run medical test for you - One click away</h3>
+                                    <p>Have you recently been tested? <br> Click Patient Log in to view your report</p>
+                                    <a href="{{ route('getSignup') }}" class="page-scroll btn btn-primary mobile-btn-hov" style="background-color: #3B5998; width: 20%; border: none;">
+                                        <i class="fa fa-user-plus"></i> Staff Sign Up
+                                    </a>
 
-                                <a href="{{ route('getLogin') }}" class="page-scroll btn btn-primary mobile-btn-hov" style="background-color: #3B5998; width: 20%; border: none;">
-                                    <i class="fa fa-sign-in"></i> Staff Sign In
-                                </a>
+                                    <a href="{{ route('getLogin') }}" class="page-scroll btn btn-primary mobile-btn-hov" style="background-color: #3B5998; width: 20%; border: none;">
+                                        <i class="fa fa-sign-in"></i> Staff Sign In
+                                    </a>
 
-                                <a href="{{ route('getPatientLogin') }}" class="page-scroll btn btn-primary mobile-btn-hov" style="background-color: #3B5998; width: 20%; border: none;">
-                                    <i class="fa fa-sign-in"></i> Patient Log In
-                                </a>
+                                    <a href="{{ route('getPatientLogin') }}" class="page-scroll btn btn-primary mobile-btn-hov" style="background-color: #3B5998; width: 20%; border: none;">
+                                        <i class="fa fa-sign-in"></i> Patient Log In
+                                    </a>
                                 @else
-                                <a href="{{ route('getAdminPage') }}" class="page-scroll btn btn-primary mobile-btn-hov" style="background-color: #3B5998; width: 20%; border: none;">
-                                    <i class="fa fa-tachometer"></i> Dashboard
-                                </a>
-                                <a href="{{ route('logout') }}" class="page-scroll btn btn-primary mobile-btn-hov" style="background-color: #3B5998; width: 20%; border: none;">
-                                    <i class="fa fa-sign-out"></i> Logout
-                                </a>
+                                    <h3>Welcome {{ Auth::user()->first_name }}</h3>
+                                    <p> You will need to be given an admin access to see your dashboard </p>
+                                    <a href="{{ route('getAdminPage') }}" class="page-scroll btn btn-primary mobile-btn-hov" style="background-color: #3B5998; width: 20%; border: none;">
+                                        <i class="fa fa-tachometer"></i> Dashboard
+                                    </a>
+                                    <a href="{{ route('logout') }}" class="page-scroll btn btn-primary mobile-btn-hov" style="background-color: #3B5998; width: 20%; border: none;">
+                                        <i class="fa fa-sign-out"></i> Logout
+                                    </a>
                                 @endif
                             </div>
                         </div>
